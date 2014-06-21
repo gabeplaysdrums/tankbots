71b33ea0d47d4653880745d1c93f7ab09a60c867
Gabriel Young <gabeplaysdrums@live.com>
2014-06-20 20:52:05 -0700

    template for the main view

--- tankbots/urls.py
+++ tankbots/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tankbots.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'tankbots.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
